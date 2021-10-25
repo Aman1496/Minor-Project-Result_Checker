544859cbba87343f33a1b5ab750bf55d01ede058
Aman1496 <69244562+Aman1496@users.noreply.github.com>
2021-10-25 12:29:08 +0530

    Delete page.py

--- page.py
+++
@@ -1,9 +0,0 @@
-from pymongo import MongoClient
-client = MongoClient("mongodb+srv://manasvi:man14@rc.jxjlg.mongodb.net/test")
-db = client["dbname"]
-try:
- db.command("serverStatus")
-except Exception as e: print(e)
-else: print("You are connected!")
-client.close()
-
